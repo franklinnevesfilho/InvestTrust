2ba67c34ecfbf8caa73312783a0727adaa23dae2
franklinnevesfilho <franklin.neves.filho@gmail.com>
2022-09-12 00:45:52 -0500

    init

---
+++ subsciptionmanager/src/main/java/com/investtrust/subsciptionmanager/SubsciptionmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.investtrust.subsciptionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsciptionmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsciptionmanagerApplication.class, args);
+	}
+
+}
---
+++ subsciptionmanager/src/test/java/com/investtrust/subsciptionmanager/SubsciptionmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investtrust.subsciptionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsciptionmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
