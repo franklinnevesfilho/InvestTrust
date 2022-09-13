a5bda9c49c12f8d4ee705ba97d80bf73e67dc815
franklinnevesfilho <franklin.neves.filho@gmail.com>
2022-09-13 07:40:03 -0500

    CRUD2

--- subscriptionmanagerapp/src/app/app-routing.module.ts
+++ subscriptionmanagerapp/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './components/employee/employee.component';
-import { TestComponent } from './components/test/test.component';
 
 const routes: Routes = [
   { path: '', component: EmployeeComponent },
-  { path: 'Test', component: TestComponent}
 ];
 
 @NgModule({
